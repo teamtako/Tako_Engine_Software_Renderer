64d2d5e2432d72ce2cee0da3902156b8fc4f612c
RobotOnFire <robotronfire@gmail.com>
2019-06-09 11:41:40 -0700

    Uploaded Code
    weweweweweweee

--- src/math/Matrix3.java
+++ src/math/Matrix3.java
@@ -4,6 +4,14 @@ public class Matrix3 {
 
 	public float[][] matrix;
 	
+	public static Matrix3 add(Matrix3 mat1, Matrix3 mat2) {
+		return add(mat1, mat2);
+	}
+	
+	public static Matrix3 subtract(Matrix3 mat1, Matrix3 mat2) {
+		return subtract(mat1, mat2);
+	}
+	
 	public Matrix3() {
 		matrix = new float[3][3];
 		for (int i = 0; i < matrix.length; i++) {
@@ -13,10 +21,50 @@ public class Matrix3 {
 		}
 	}
 
+	public static Matrix3 multiply(Matrix3 mat1, Matrix3 mat2) {
+		Matrix3 m = new Matrix3();
+		m.matrix[0][0] = (mat1.matrix[0][0] * mat2.matrix[0][0]) + (mat1.matrix[1][0] * mat2.matrix[0][1]) + (mat1.matrix[2][0] * mat2.matrix[0][2]);
+		m.matrix[0][1] = (mat1.matrix[0][1] * mat2.matrix[0][0]) + (mat1.matrix[1][1] * mat2.matrix[0][1]) + (mat1.matrix[2][1] * mat2.matrix[0][2]);
+		m.matrix[0][2] = (mat1.matrix[0][2] * mat2.matrix[0][0]) + (mat1.matrix[1][2] * mat2.matrix[0][1]) + (mat1.matrix[2][2] * mat2.matrix[0][2]);
+		
+		m.matrix[1][0] = (mat1.matrix[0][0] * mat2.matrix[1][0]) + (mat1.matrix[1][0] * mat2.matrix[1][1]) + (mat1.matrix[2][0] * mat2.matrix[1][2]);
+		m.matrix[1][1] = (mat1.matrix[0][1] * mat2.matrix[1][0]) + (mat1.matrix[1][1] * mat2.matrix[1][1]) + (mat1.matrix[2][1] * mat2.matrix[1][2]);
+		m.matrix[1][2] = (mat1.matrix[0][2] * mat2.matrix[1][0]) + (mat1.matrix[1][2] * mat2.matrix[1][1]) + (mat1.matrix[2][2] * mat2.matrix[1][2]);
+		
+		m.matrix[2][0] = (mat1.matrix[0][0] * mat2.matrix[2][0]) + (mat1.matrix[1][0] * mat2.matrix[2][1]) + (mat1.matrix[2][0] * mat2.matrix[2][2]);
+		m.matrix[2][1] = (mat1.matrix[0][1] * mat2.matrix[2][0]) + (mat1.matrix[1][1] * mat2.matrix[2][1]) + (mat1.matrix[2][1] * mat2.matrix[2][2]);
+		m.matrix[2][2] = (mat1.matrix[0][2] * mat2.matrix[2][0]) + (mat1.matrix[1][2] * mat2.matrix[2][1]) + (mat1.matrix[2][2] * mat2.matrix[2][2]);
+		return m;
+	}
+	
+	public static Matrix3 divide(Matrix3 mat1, Matrix3 mat2) {
+		Matrix3 m = new Matrix3();
+		m.matrix[0][0] = (mat1.matrix[0][0] / mat2.matrix[0][0]) + (mat1.matrix[1][0] / mat2.matrix[0][1]) + (mat1.matrix[2][0] / mat2.matrix[0][2]);
+		m.matrix[0][1] = (mat1.matrix[0][1] / mat2.matrix[0][0]) + (mat1.matrix[1][1] / mat2.matrix[0][1]) + (mat1.matrix[2][1] / mat2.matrix[0][2]);
+		m.matrix[0][2] = (mat1.matrix[0][2] / mat2.matrix[0][0]) + (mat1.matrix[1][2] / mat2.matrix[0][1]) + (mat1.matrix[2][2] / mat2.matrix[0][2]);
+		
+		m.matrix[1][0] = (mat1.matrix[0][0] / mat2.matrix[1][0]) + (mat1.matrix[1][0] / mat2.matrix[1][1]) + (mat1.matrix[2][0] / mat2.matrix[1][2]);
+		m.matrix[1][1] = (mat1.matrix[0][1] / mat2.matrix[1][0]) + (mat1.matrix[1][1] / mat2.matrix[1][1]) + (mat1.matrix[2][1] / mat2.matrix[1][2]);
+		m.matrix[1][2] = (mat1.matrix[0][2] / mat2.matrix[1][0]) + (mat1.matrix[1][2] / mat2.matrix[1][1]) + (mat1.matrix[2][2] / mat2.matrix[1][2]);
+		
+		m.matrix[2][0] = (mat1.matrix[0][0] / mat2.matrix[2][0]) + (mat1.matrix[1][0] / mat2.matrix[2][1]) + (mat1.matrix[2][0] / mat2.matrix[2][2]);
+		m.matrix[2][1] = (mat1.matrix[0][1] / mat2.matrix[2][0]) + (mat1.matrix[1][1] / mat2.matrix[2][1]) + (mat1.matrix[2][1] / mat2.matrix[2][2]);
+		m.matrix[2][2] = (mat1.matrix[0][2] / mat2.matrix[2][0]) + (mat1.matrix[1][2] / mat2.matrix[2][1]) + (mat1.matrix[2][2] / mat2.matrix[2][2]);
+		return m;
+	}
+	
 	public void add(Matrix3 mat) {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix.length; j++) {
-				matrix[i][j] = mat.matrix[i][j];
+				matrix[i][j] += mat.matrix[i][j];
+			}
+		}
+	}
+	
+	public void subtract(Matrix3 mat) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				matrix[i][j] -= mat.matrix[i][j];
 			}
 		}
 	}
--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -1,5 +1,6 @@
 package runner;
 
+import math.Matrix4;
 import renderEngine.Display;
 
 public class Runner {
@@ -7,6 +8,46 @@ public class Runner {
 	Display display;
 	
 	public Runner() {
+		Matrix4 mat1 = new Matrix4();
+		Matrix4 mat2 = new Matrix4();
+		for (int i = 0; i < mat1.matrix.length; i++) {
+			for (int j = 0; j < mat1.matrix[i].length; j++) {
+				if(i % 2 == 0 && j % 2 == 0) {
+					mat1.matrix[i][j] = 2;
+				}else {
+				mat1.matrix[i][j] = 1;
+				}
+				mat2.matrix[i][j] = 1;
+			}
+		}
+		
+		Matrix4 mat3 = Matrix4.multiply(mat1, mat2);
+		
+		for (int i = 0; i < mat1.matrix.length; i++) {
+			for (int j = 0; j < mat1.matrix[i].length; j++) {
+				System.out.print(mat1.matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < mat1.matrix.length; i++) {
+			for (int j = 0; j < mat1.matrix[i].length; j++) {
+				System.out.print(mat2.matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for (int i = 0; i < mat1.matrix.length; i++) {
+			for (int j = 0; j < mat1.matrix[i].length; j++) {
+				System.out.print(mat3.matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
 		display = new Display(720, 720);
 		display.clearBackBuffer(0, 0, 0, 1);
 		display.swapBuffers();
